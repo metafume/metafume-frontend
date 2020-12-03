5fb182c220e0585c1d680be978ad3f67a66be5ce
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-03 21:39:36 +0900

    chore: initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'comma-dangle': ['warn', 'always-multiline'],
+    'arrow-parens': ['warn', 'as-needed'],
+    'eol-last': ['warn', 'always'],
+    'no-unused-vars': ['warn', { args: 'none' }],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+    'space-before-function-paren': [
+      'warn',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'space-before-blocks': ['warn', 'always'],
+  },
+};
