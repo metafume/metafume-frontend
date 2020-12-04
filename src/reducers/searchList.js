800a0146651350eb7dbfc598d3a61b4a817afa96
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-04 18:20:32 +0900

    wip: update searchList reducer

--- src/reducers/searchList.js
+++ src/reducers/searchList.js
@@ -1,5 +1,11 @@
+import * as types from '../constants/actionTypes';
+
 const searchListReducer = (state = null, action) => {
   switch(action.type) {
+    case types.ADD_SEARCH_LIST:
+      return action.payload.list;
+    case types.DELETE_SEARCH_LIST:
+      return null;
     default:
       return state;
   }
