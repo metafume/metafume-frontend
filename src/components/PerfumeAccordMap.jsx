80a96b9251b6cc87b6705b60d20c5b108debdd93
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-07 21:34:25 +0900

    feat: update interaction of perfume's accord map

--- src/components/PerfumeAccordMap.jsx
+++ src/components/PerfumeAccordMap.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { select, drag, randomUniform, easeCircleInOut } from 'd3';
+import { select, drag, randomUniform, easeCircleInOut, easeCubic, easeQuadOut } from 'd3';
+
+const imageStyleOption = {
+  position: 'absolute',
+  width: '260px',
+  bottom: '60px',
+  left: '50%',
+  transform: 'translate(-50%)',
+  mixBlendMode: 'multiply',
+};
 
 const dragEventListener = drag()
   .on('start', function () {
@@ -12,6 +21,28 @@ const dragEventListener = drag()
   .on('end', function () {
     select(this).raise().attr('stroke', null);
   });
+
+const animateCircle = circle => {
+  const updateCircle = circle
+    .transition()
+    .ease(easeCubic)
+    .duration(40000)
+    .attr('opacity', randomUniform(0.5, 1))
+    .attr('cx', randomUniform(-200, 500))
+    .attr('cy', randomUniform(20, 260))
+    .on('end', () => updateCircle.call(animateCircle));
+};
+
+const handleMouseOver = function () {
+  const self = select(this);
+  self
+    .transition()
+    .ease(easeQuadOut)
+    .duration(1200)
+    .style('opacity', randomUniform(0.5, 1))
+    .style('transform', `translate(${randomUniform(-40, 40)()}px, ${randomUniform(-40, 40)()}px)`)
+    .on('end', () => self.call(animateCircle));
+};
 
 const AccordMap = ({ accords, imageUrl, name }) => {
   const svgRef = useRef();
@@ -26,25 +57,27 @@ const AccordMap = ({ accords, imageUrl, name }) => {
 
     for (let i = 0; i < 30; i++) {
       accords.forEach(accord => {
-        svg.append('circle')
+        const circle = svg.append('circle')
           .attr('r', 0)
           .attr('cx', randomUniform(-200, 500))
           .attr('cy', randomUniform(20, 260))
           .attr('fill', accord.styles.background)
           .attr('opacity', 0)
           .call(dragEventListener)
+          .on('mouseover', handleMouseOver)
           .transition()
           .ease(easeCircleInOut)
           .duration(randomUniform(1200, 2000))
           .attr('r', 40 * parseInt(accord.styles.width) / 200)
-          .attr('opacity', 0.8);
+          .attr('opacity', 0.9)
+          .on('end', () => circle.call(animateCircle));
       });
     }
   }, []);
 
   return (
     <div style={{ position: 'relative' }}>
-      <img style={{ position: 'absolute', width: '260px', bottom: '60px', left: '50%', transform: 'translate(-50%)', mixBlendMode: 'multiply' }} src={imageUrl} alt={name}/>
+      <img style={imageStyleOption} src={imageUrl} alt={name}/>
       <svg ref={svgRef}/>
     </div>
   );
