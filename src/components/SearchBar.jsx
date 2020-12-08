debf0c98b66a3862d6fbbdab0c589c4d1fb0c489
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-08 20:17:54 +0900

    CSS: update styles in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,23 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Button from './Button';
+
+const StyledForm = styled.form`
+  width: inherit;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledInput = styled.input`
+  all: unset;
+  width: 80%;
+  border-bottom: 0.4px solid lightgray;
+  padding: 6px;
+  margin-right: 24px;
+  font-size: 24px;
+`;
 
 const SearchBar = ({ onSearch, onResetSearch }) => {
   const [value, setValue] = useState('');
@@ -21,10 +39,15 @@ const SearchBar = ({ onSearch, onResetSearch }) => {
   };
 
   return (
-    <form onSubmit={handleOnSubmit}>
-      <input type='search' placeholder='search' value={value} onChange={handleOnChange} />
-      <button onClick={handleOnSubmit}>Search</button>
-    </form>
+    <StyledForm onSubmit={handleOnSubmit}>
+      <StyledInput
+        type='search'
+        placeholder='search'
+        value={value}
+        onChange={handleOnChange}
+      />
+      <Button onClick={handleOnSubmit}>Find</Button>
+    </StyledForm>
   );
 };
 
