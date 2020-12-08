82e77f106bab8c66ae92c900ca12d21e943b97a0
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-08 17:00:14 +0900

    chore: set initial configuration for styling

---
+++ src/components/styles/GlobalSyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Playfair Display', serif;
+    font-weight: 700;
+  }
+`;
+
+export default GlobalStyle;
