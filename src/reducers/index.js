4e7d14daa4f84e3731487976fb9e9cedbe3e7244
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-09 12:09:08 +0900

    chore: drop product reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user';
-import productReducer from './product';
 import searchListReducer from './searchList';
 
 export default combineReducers({
   user: userReducer,
-  product: productReducer,
   searchList: searchListReducer,
 });
