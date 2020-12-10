164019e61e7d2d9214aefb5890921b789934d8dc
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-10 18:37:30 +0900

    feat: add user saga for handling api call

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,7 +1,11 @@
 import { all, call } from 'redux-saga/effects';
 
 import searchListSagas from './searchList.saga';
+import userSagas from './user.saga';
 
 export default function* saga() {
-  yield all([call(searchListSagas)]);
+  yield all([
+    call(searchListSagas),
+    call(userSagas),
+  ]);
 }
---
+++ src/sagas/user.saga.js
@@ -0,0 +1,28 @@
+import { takeLatest, put, all, call } from 'redux-saga/effects';
+import * as actions from '../reducers/user';
+
+import api from '../utils/api';
+
+function* login({ payload }) {
+  try {
+    const { token } = payload;
+    let user = null;
+
+    if (token) user = yield api.tokenLogin(token);
+    else yield user = yield api.googleLogin();
+
+    yield put(actions.loginSuccess(user));
+  } catch (err) {
+    yield put(actions.loginFailure(err));
+  }
+}
+
+function* watchLoginStart() {
+  yield takeLatest(actions.loginStart.type, login);
+}
+
+export default function* userSagas() {
+  yield all([
+    call(watchLoginStart),
+  ]);
+}
