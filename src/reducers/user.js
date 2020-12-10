acd2f4b857789cb91664058da2a7d7a525f494fe
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-10 18:36:44 +0900

    feat: update user reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,8 +1,51 @@
-const userReducer = (state = null, action) => {
-  switch(action.type) {
-    default:
-      return state;
-  }
+import { createSlice } from '@reduxjs/toolkit';
+
+const LOGIN_USER_START = 'LOGIN_USER_START';
+const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
+const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE';
+
+const initialState = {
+  profile: null,
+  loading: false,
+  error: null,
 };
 
-export default userReducer;
+const reducers = {
+  [LOGIN_USER_START]: {
+    reducer: state => ({
+      ...state,
+      loading: true,
+      error: null,
+    }),
+    prepare: token => ({ payload: { token } }),
+  },
+  [LOGIN_USER_SUCCESS]: {
+    reducer: (state, action) => ({
+      ...state,
+      profile: action.payload.user,
+      loading: false,
+      error: null,
+    }),
+    prepare: user => ({ payload: { user }}),
+  },
+  [LOGIN_USER_FAILURE]: {
+    reducer: (state, action) => ({
+      ...state,
+      loading: false,
+      error: action.payload.error,
+    }),
+    prepare: error => ({ payload: { error }}),
+  },
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers,
+});
+
+export const loginStart = userSlice.actions[LOGIN_USER_START];
+export const loginSuccess = userSlice.actions[LOGIN_USER_SUCCESS];
+export const loginFailure = userSlice.actions[LOGIN_USER_FAILURE];
+
+export default userSlice.reducer;
