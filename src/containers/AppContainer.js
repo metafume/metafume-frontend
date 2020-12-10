6f85821f803ece4311965a334c3748fef979de94
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-10 18:38:55 +0900

    update: add onLoad, onLogin methods in AppContainer

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,12 +2,22 @@ import { connect } from 'react-redux';
 
 import App from '../components/App';
 
+import { loginStart } from '../reducers/user';
+import { getCookie } from '../utils/helpers';
+
+
 const mapStateToProps = state => ({
-  product: state.product,
+  user: state.user?.profile,
 });
 
-const mapDispatchToProps = dispatch => {
-  return {};
-};
+const mapDispatchToProps = dispatch => ({
+  onLoad: () => {
+    const token = getCookie();
+    if (token) dispatch(loginStart(token));
+  },
+  onLogin: () => {
+    dispatch(loginStart());
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
