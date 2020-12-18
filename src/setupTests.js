0a6c1f717e853b9ae2bbdeea5476a5781728bea6
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-18 17:52:12 +0900

    chore: update test configuration

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,6 @@
+import React from 'react';
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+export const renderWithRouter = component => render(<Router>{component}</Router>);
