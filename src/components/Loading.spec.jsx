51be737feb8ca50db650826a65ad4be10b45a0a1
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-18 22:46:44 +0900

    test: update test code in Loading.spec.jsx

--- src/components/Loading.spec.jsx
+++ src/components/Loading.spec.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Loading from './Loading';
 
 describe('<Loading />', () => {
-  afterEach(cleanup);
-
-  it('renders loading', () => {
-    const { getByText } = render(<Loading />);
-    expect(getByText(/Loading/)).toBeInTheDocument();
-  });
-
   it('shows the props correctly', () => {
     const ERROR_MESSAGE = 'Network Error';
     const { getByText } = render(<Loading>{ERROR_MESSAGE}</Loading>);
