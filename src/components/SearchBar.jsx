26ed419b818974cf5015905b912cb34cb10c593a
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2021-01-13 10:49:50 +0900

    chore: remove unnecessary import about React

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
